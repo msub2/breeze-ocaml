bca214a3b190b71e0d002b5dc5a8c57fc701a45a
Daniel Adams <msub2official@gmail.com>
2024-10-16 14:13:54 -1000

    Start adding more protocol handling

--- bin/main.ml
+++ bin/main.ml
@@ -11,12 +11,16 @@ let _height = ref 480
 let go_action gopher_view urlbar = 
   let url = Widget.get_text urlbar in
   History.add_entry (url, Gophermap);
-  let (host, port, selector) = match parse_url url with
-  | Success (host, port, request_body) -> (host, port, request_body)
-  | Failure _ -> ("gopher.floodgap.com", 70, "\r\n") in
+  let (host, port, selector, protocol) = match parse_url url with
+  | Success (host, port, request_body, protocol) -> (host, port, request_body, protocol)
+  | Failure _ -> ("gopher.floodgap.com", 70, "\r\n", Gopher) in
   let request_body = selector ^ "\r\n" in
   let response = try network_request host port request_body with Failure message -> message in
-  parse_gopher_response response gopher_view urlbar
+  match protocol with
+  | Gopher -> parse_gopher_response response gopher_view urlbar
+  | Gemini -> parse_plaintext_response response gopher_view
+  | _ -> parse_plaintext_response response gopher_view
+  
 
 let history_action (action : history_action) gopher_view urlbar = 
   let can_navigate = match action with 
@@ -28,9 +32,9 @@ let history_action (action : history_action) gopher_view urlbar =
     | Forward -> History.history_forward ()
     | Back -> History.history_back () in
     let (url, content_type) = History.get_history () in
-    let (host, port, selector) = match parse_url url with
-    | Success (host, port, request_body) -> (host, port, request_body)
-    | Failure _ -> ("gopher.floodgap.com", 70, "\r\n") in
+    let (host, port, selector, _) = match parse_url url with
+    | Success (host, port, request_body, protocol) -> (host, port, request_body, protocol)
+    | Failure _ -> ("gopher.floodgap.com", 70, "\r\n", Gopher) in
     let request_body = selector ^ "\r\n" in
     let response = try network_request host port request_body with Failure message -> message in
     let _ = match content_type with
@@ -50,7 +54,7 @@ let () =
   let gopher_view = gopherview_widget
     |> Layout.resident ~w:!_width ~h:!_height
     |> Layout.make_clip ~w:!_width ~h:!_height in
-  let urlbar = Widget.text_input ~text:"gopher://gopher.floodgap.com" ~prompt:"Enter URL..." () ~size:16 in
+  let urlbar = Widget.text_input ~text:"gemini://geminiprotocol.net/docs/protocol-specification.gmi" ~prompt:"Enter URL..." () ~size:16 in
   let go_button = Widget.button "Go" ~action:(fun _ -> go_action gopher_view urlbar) in
   let back_button = Widget.button "<" ~action:(fun _ -> history_action Back gopher_view urlbar) in
   let forward_button = Widget.button ">" ~action:(fun _ -> history_action Forward gopher_view urlbar) in
--- bin/url.ml
+++ bin/url.ml
@@ -1,3 +1,5 @@
+open Protocols
+
 (** Parsing/Validation for URLs *)
 
 type parse_error =
@@ -8,7 +10,7 @@ type parse_error =
     - Success: Returns URL, port, request body
     - Failure: Returns a parsing error *)
 type parse_result =
-  | Success of string * int * string 
+  | Success of string * int * string * protocol
   | Failure of parse_error
 
 (** Extracts the host, port, and selector from a gopher URL.
@@ -17,7 +19,16 @@ let parse_gopher_url url =
   match String.split_on_char '/' url with
   | host :: selector_parts -> 
     let request_body = String.concat "/" selector_parts in
-    Success (host, 70, request_body)
+    Success (host, 70, request_body, Gopher)
+  | [] -> Failure Malformed
+
+(** Extracts the host, port, and selector from a gopher URL.
+    This assumes that the gopher:// prefix has already been stripped. *)
+let parse_gemini_url url =
+  (* TODO: More stringent parsing *)
+  match String.split_on_char '/' url with
+  | host :: _ -> 
+    Success (host, 1965, "gemini://" ^ url, Gemini)
   | [] -> Failure Malformed
 
 (** Takes in a URL and attempts to extract the necessary parameters to pass
@@ -25,4 +36,5 @@ let parse_gopher_url url =
 let parse_url url = 
   match url with
   | url when String.starts_with ~prefix:"gopher://" url -> parse_gopher_url (String.sub url 9 (String.length url - 9))
+  | url when String.starts_with ~prefix:"gemini://" url -> parse_gemini_url (String.sub url 9 (String.length url - 9))
   | _ ->  "Bad URL: " ^ url |> failwith;
